6447564bfaba1ba4780d7f1fd82f24c74cacaa61
malashenkoEvgeniy <111k1808@gmail.com>
2022-01-18 23:21:29 +0200

    insert admin Lte

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -12,11 +12,17 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
+        'adminLte/dist/css/AdminLTE.min.css',
+        'adminLte/yii2-fontawesome/css/font-awesome.min.css',
         'css/site.css',
     ];
     public $js = [
+        'adminLte/dist/js/adminlte.min.js',
     ];
     public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
         'yii\web\YiiAsset',
         'yii\bootstrap4\BootstrapAsset',
     ];
