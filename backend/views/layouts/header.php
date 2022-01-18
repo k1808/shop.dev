e9b39270843034be4ba364745ba186c385f813da
malashenkoEvgeniy <111k1808@gmail.com>
2022-01-19 00:18:20 +0200

    Changed login layout2

--- backend/views/layouts/header.php
+++ backend/views/layouts/header.php
@@ -17,7 +17,7 @@ use yii\helpers\Html;
 
         <div class="navbar-custom-menu">
 
-            <ul class="nav navbar-nav">
+            <ul class="nav navbar-nav" style="display: contents">
 
                 <!-- Messages: style can be found in dropdown.less-->
                 <li class="dropdown messages-menu">
