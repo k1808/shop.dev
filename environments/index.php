285079bfef5064dc5fff2afa1477441ad9cf2ad0
malashenkoEvgeniy <111k1808@gmail.com>
2022-01-19 00:01:32 +0200

    Separated UrlManagers

--- environments/index.php
+++ environments/index.php
@@ -32,6 +32,7 @@ return [
     'Development' => [
         'path' => 'dev',
         'setWritable' => [
+            'common/runtime',
             'backend/runtime',
             'backend/web/assets',
             'console/runtime',
@@ -49,6 +50,7 @@ return [
     'Production' => [
         'path' => 'prod',
         'setWritable' => [
+            'common/runtime',
             'backend/runtime',
             'backend/web/assets',
             'console/runtime',
